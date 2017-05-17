cbe1c295372273377c5d4280e422b4cb796eef77
Blueibis <jennie.li.2011@gmail.com>
2017-05-17 14:09:31 -0400

    Create exercise.rb - complete exercise 5

---
+++ exercise.rb
@@ -0,0 +1,12 @@
+#Method Exercise 5
+def greet_backwards(input)
+  input += input
+  puts "Hello #{input.reverse}. Welcome home! I swear it was the dog's fault."
+end
+
+greet_backwards("Bob")
+greet_backwards("Shirly")
+greet_backwards("Sue")
+greet_backwards("Andy")
+
+#Method Exercise 6
